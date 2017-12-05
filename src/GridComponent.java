5788936e472a68b6a436bdb82946e3035f935182
Jason Harnack <jfh79@case.edu>
2017-12-05 11:16:16 -0500

    Adding threads

--- src/GridComponent.java
+++ src/GridComponent.java
@@ -1,5 +1,8 @@
 
 public interface GridComponent {
 	//put whatever tf the enum and car class have in common here
+	int rowLocation = 0;
+	int columnLocation = 0;
+	
 	public String print();
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,20 +2,33 @@ import java.util.ArrayList;
 
 public class Runner {
 	private static Grid grid;
+	private static boolean isMoving;
+	private static boolean isAbleToMove;
 	
 	public static void main (String[] args) {
 		grid = new Grid(Integer.parseInt(args[0]));
 		runThreads(grid.getCarList(), grid.getLightList());
 	}
 	
-	public static void runThreads(ArrayList<GridCar> listOfCars, ArrayList<Grid.GridEnum> listOfLights) {
+	public static void runThreads (ArrayList<GridCar> listOfCars, ArrayList<Grid.GridEnum> listOfLights) {
 		for(GridCar car : listOfCars) {
 			//start a thread and move them around and stuff
 			//do something like Car.getSpeed() to get the speed and increment the car that many spaces
+			new Thread() {
+				public void run() {
+					if(isAbleToMove)
+						car.move();
+				}
+			}.start();
 		}
 		
 		for(Grid.GridEnum stoplight : listOfLights) {
 			//start a thread and change them every few seconds
+			new Thread() {
+				public void run() {
+					
+				}
+			}.start();
 		}
 	}
 	
