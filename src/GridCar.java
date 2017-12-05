6b49befaea52202c60cd6bab6f48c092f1e2c2f4
Vicki Kelly <vrk12@case.edu>
2017-12-05 00:36:28 -0500

    deleting unnecessary src file

--- src/GridCar.java
+++
@@ -1,9 +0,0 @@
-
-public class GridCar implements GridComponent {
-	//put whatever tf in here that a car needs
-	
-	@Override
-	public String print() {
-		return "C";
-	}
-}
--- src/GridComponent.java
+++
@@ -1,5 +0,0 @@
-
-public interface GridComponent {
-	//put whatever tf the enum and car class have in common here
-	public String print();
-}
