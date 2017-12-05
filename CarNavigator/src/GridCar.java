24d6d245d42948101cfd49620906027adfc2fc06
Vicki Kelly <vrk12@case.edu>
2017-12-04 23:53:07 -0500

    adds comments and the skeleton of some things that can change based on design decisions

--- CarNavigator/src/GridCar.java
+++ CarNavigator/src/GridCar.java
@@ -16,4 +16,11 @@ public class GridCar implements GridComponent {
 	public String print() {
 		return "C";
 	}
+	
+	public void move(int spaces) {
+		//is this the approach we want to take?
+		//we can either tell the grid the new position and the grid tells that to the car
+		//or we can tell the car its new position from the thread and then have it tell the grid
+		//we could do this by storing old and new location in car and have it clear out the old location in the grid
+	}
 }
--- CarNavigator/src/Runner.java
+++ CarNavigator/src/Runner.java
@@ -5,10 +5,10 @@ public class Runner {
 	
 	public static void main (String[] args) {
 		grid = new Grid(Integer.parseInt(args[0]));
-		startThreads(grid.getCarList(), grid.getLightList());
+		runThreads(grid.getCarList(), grid.getLightList());
 	}
 	
-	public static void startThreads(ArrayList<GridCar> listOfCars, ArrayList<Grid.GridEnum> listOfLights) {
+	public static void runThreads(ArrayList<GridCar> listOfCars, ArrayList<Grid.GridEnum> listOfLights) {
 		for(GridCar car : listOfCars) {
 			//start a thread and move them around and stuff
 			//do something like Car.getSpeed() to get the speed and increment the car that many spaces
@@ -16,6 +16,20 @@ public class Runner {
 		
 		for(Grid.GridEnum stoplight : listOfLights) {
 			//start a thread and change them every few seconds
+		}
+	}
+	
+	
+	private class CarThread extends Thread{
+		GridCar car; //idk if this is how we want to do this or nah
+		int speed = car.getSpeed();
+		
+		public void run(GridCar car) {
+			this.car = car;
+			
+			while (true) {
+				//increment position based on speed
+			}
 		}
 	}
 }
